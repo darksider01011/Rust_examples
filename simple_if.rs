a4773bed7a3315728c5cadad89a462eac28d9b97
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2025-06-29 05:42:50 -0700

    Create simple_if.rs

---
+++ simple_if.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let x: u8 = 10;
+    
+    if (x > 10) {
+        println!("creater than 10");
+    } else if (x == 10){
+        println!("equal");
+    }else {
+        println!("less than 10")
+    }
+
+    // output: equal
+
+}
