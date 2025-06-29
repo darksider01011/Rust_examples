c9195f9e2ac925098e03ed3a1725b7ee2105bda7
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2025-06-29 04:55:04 -0700

    Create statment_vs_expression.rs

---
+++ statment_vs_expression.rs
@@ -0,0 +1,12 @@
+fn main() {
+ //statement (ended with semicolon)
+ let y = {
+
+    //expression (return value)
+    let x = 3;
+    x + 1
+    };
+    
+    //output: The value of y is: 4
+    println!("The value of y is: {y}");
+ }
